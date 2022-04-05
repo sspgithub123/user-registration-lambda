ea2468482f84e132e034ca3a7377760c6f2ed1e4
Shubham <ssp671995@gmail.com>
2022-04-05 11:15:49 +0530

    Added the welcome message in lambda validation

---
+++ src/com/bridgelabz/LambdaValidation.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+/**
+ * Program on User Registration Using Regex
+ *
+ * @author : Shubham Pawar
+ * @since : 04/04/2022
+ * */
+
+
+public class LambdaValidation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the lambda validation program");
+    }
+}
