8fb8d84d6b4419ee4e107031dd7ccfed630038c7
sspgithub123 <99379178+sspgithub123@users.noreply.github.com>
2022-04-05 12:28:46 +0530

    Update UserRegistration.java

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -23,24 +23,5 @@ public class UserRegistration {
         };
         System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Shubham"));
 
-        Validation isValidLastName = (pattern, lastName) -> {
-            return "Last Name is " + Pattern.compile(pattern).matcher(lastName).matches();
-        };
-        System.out.println(isValidLastName.validate("^[A-Z]{1}[a-z]{2,}$", "Pawar"));
-
-        Validation isValidEmail = (pattern, email) -> {
-            return "Email is " + Pattern.compile(pattern).matcher(email).matches();
-        };
-        System.out.println(isValidEmail.validate("^[a-z]+[+-_.]*[a-z]*[@][a-z]+[.][a-z]{2,4}[.]*([a-z]{2})*$",
-                "Shubham.shu@gmail.com"));
-        Validation isValidMobileNumber = (pattern, mobileNumber) -> {
-            return "Mobile number is " + Pattern.compile(pattern).matcher(mobileNumber).matches();
-        };
-        System.out.println(isValidMobileNumber.validate("^[91]+[ ]?[6-9]{1}[0-9]{9}$", "91 8530705596"));
-        Validation isValidPassword1 = (pattern, password) -> {
-            return "Password rule is " + Pattern.compile(pattern).matcher(password).matches();
-        };
-        System.out.println(isValidPassword1.validate("^[0-9a-zA-Z!,@#$&*().]{8,}$", "Shubham3@"));
-
     }
 }
