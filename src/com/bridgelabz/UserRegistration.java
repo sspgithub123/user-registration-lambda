b8a5ccffe30c3a5d32db558f36c69cebfa8489bd
sspgithub123 <99379178+sspgithub123@users.noreply.github.com>
2022-04-05 11:27:04 +0530

    Update UserRegistration.java

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -21,7 +21,7 @@ public class UserRegistration {
         Validation isValidFirstName = (pattern, firstName) -> {
             return "First Name is " + Pattern.compile(pattern).matcher(firstName).matches();
         };
-        System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Ashvini"));
+        System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Shubham"));
 
     }
 }
